e5ceffcd4b309be95cab09e40afb298b3e0f41e5
Julia Alexeeva <julia4406@gmail.com>
2025-02-25 09:11:24 +0200

    Fix: settings.py, requirements.txt (dotenv and SECURITY_KEY modifications)

--- src/train_ticket_service/settings.py
+++ src/train_ticket_service/settings.py
@@ -14,15 +14,19 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4b+b_)2xloubdm5$$gu)0$r8-+=y&m2yny91!+up#pp6" "=-u4"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
