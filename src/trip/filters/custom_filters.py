78f343161d8fe34a5744178d4b9ea00cb75a437c
Julia Alexeeva <julia4406@gmail.com>
2025-03-05 15:16:29 +0200

    Refactor filtering (add separate func)

--- src/trip/filters/custom_filters.py
+++ src/trip/filters/custom_filters.py
@@ -2,119 +2,124 @@ import django_filters
 from django.db.models import Q
 
 
+def get_filtered_queryset(queryset, value, field_params):
+    """
+    Filters queryset based on multiple fields and lookup expressions.
+    """
+    if not value:
+        return queryset
+
+    values_list = value.split(",")
+    query = Q()
+    for item in values_list:
+        for field, lookup in field_params:
+            query |= Q(**{f"{field}__{lookup}": item})
+    return queryset.filter(query).distinct()
+
+
 class CitiesRouteFilter(django_filters.CharFilter):
     """Filters by cities (source or destination)"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        city_list = value.split(",")
-        query = Q()
-        for city in city_list:
-            query |= Q(source__name__icontains=city) | Q(
-                destination__name__icontains=city
-            )
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("source__name", "icontains"),
+                ("destination__name", "icontains"),
+            ],
+        )
 
 
 class SourcesRouteFilter(django_filters.CharFilter):
     """Filters by source city"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        source_list = value.split(",")
-        query = Q()
-        for source in source_list:
-            query |= Q(source__name__icontains=source)
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("source__name", "icontains"),
+            ],
+        )
 
 
 class DestinationsRouteFilter(django_filters.CharFilter):
     """Filters by destination city"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        destination_list = value.split(",")
-        query = Q()
-        for destination in destination_list:
-            query |= Q(destination__name__icontains=destination)
-        return queryset.filter(query).distinct()
+        query = get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("destination__name", "icontains"),
+            ],
+        )
+        return queryset.filter(query).distinct() if query else queryset
 
 
 class CitiesTripFilter(django_filters.CharFilter):
     """Filters by cities (source or destination)"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        city_list = value.split(",")
-        query = Q()
-        for city in city_list:
-            query |= Q(route__source__name__icontains=city) | Q(
-                route__destination__name__icontains=city
-            )
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("route__source__name", "icontains"),
+                ("route__destination__name", "icontains"),
+            ],
+        )
 
 
 class SourcesTripFilter(django_filters.CharFilter):
     """Filters by source city"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        source_list = value.split(",")
-        query = Q()
-        for source in source_list:
-            query |= Q(route__source__name__icontains=source)
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("route__source__name", "icontains"),
+            ],
+        )
 
 
 class DestinationsTripFilter(django_filters.CharFilter):
     """Filters by destination city"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        destination_list = value.split(",")
-        query = Q()
-        for destination in destination_list:
-            query |= Q(route__destination__name__icontains=destination)
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("route__destination__name", "icontains"),
+            ],
+        )
 
 
 class CrewTripFilter(django_filters.CharFilter):
     """Filters by crew member's first or last name"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        crew_list = value.split(",")
-        query = Q()
-        for crew in crew_list:
-            query |= Q(crew__first_name__icontains=crew) | Q(
-                crew__last_name__icontains=crew
-            )
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("crew__first_name", "icontains"),
+                ("crew__last_name__name", "icontains"),
+            ],
+        )
 
 
 class TrainTripFilter(django_filters.CharFilter):
     """Filters by train"""
 
     def filter(self, queryset, value):
-        if not value:
-            return queryset
-
-        train_list = value.split(",")
-        query = Q()
-        for train in train_list:
-            query |= Q(train__name_number__icontains=train)
-        return queryset.filter(query).distinct()
+        return get_filtered_queryset(
+            queryset,
+            value,
+            [
+                ("train__name_number", "icontains"),
+            ],
+        )
