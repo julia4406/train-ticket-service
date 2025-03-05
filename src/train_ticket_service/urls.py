be30b95589ec2c980adb699be36f7c99b9770090
Julia Alexeeva <julia4406@gmail.com>
2025-03-05 14:45:18 +0200

    Rename base urls into plural in accordance to REST

--- src/train_ticket_service/urls.py
+++ src/train_ticket_service/urls.py
@@ -11,8 +11,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/trip/", include("trip.urls", namespace="trip")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/trips/", include("trip.urls", namespace="trip")),
+    path("api/users/", include("user.urls", namespace="user")),
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
